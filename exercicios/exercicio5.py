446c337c18362693035cf5682885e07efbb4e721
VictorMFL <vm859222@gmail.com>
2024-05-18 15:42:11 -0300

    add exercicio do jogo da forca

--- exercicios/exercicio5.py
+++ exercicios/exercicio5.py
@@ -1,5 +1,34 @@
-palavra_secreta = "Bicicleta"
+palavra_secreta = "bicicleta"
+letras_acertadas = ""
+numero_tentativas = 0
 
-tentativa = input("Digite uma letra: ")
+caracteries_proibidos = "1234567890/?!@#$%¨&*()_+="
 
+while True:
+    tentativa = input("Digite uma letra: ")
+    numero_tentativas += 1
 
+    if tentativa in caracteries_proibidos:
+        print("Caractere inválido.")
+    elif len(tentativa) > 1:
+        print("Digite apenas 1 caractere por vez.")
+        continue
+    elif tentativa in palavra_secreta:
+        letras_acertadas += tentativa
+    
+    palavra_formada = ""
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else: 
+            palavra_formada += "*"
+
+    print(palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        print("Parabéns! Você ganhou.")
+        print("A palavra era:" + palavra_secreta)
+        print(f"Seu numero de tentativas foi {numero_tentativas}")
+        numero_tentativas = 0
+        palavra_formada = ""
+        break
